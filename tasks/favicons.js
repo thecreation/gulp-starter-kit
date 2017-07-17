15fe531628066dc914db5d0cb01ba88a90c22a64
amazingSurge <amazingSurge@yahoo.com>
2017-07-17 21:15:35 +0800

    fix favicons path issue

--- tasks/favicons.js
+++ tasks/favicons.js
@@ -6,8 +6,11 @@ import config from '../config';
 import browser from './browser';
 import gulpif from 'gulp-if';
 import notify from 'gulp-notify';
+import replace from 'gulp-replace';
+import plumber from 'gulp-plumber';
+import notifier from 'node-notifier';
 
-gulp.task('favicons', () => {
+gulp.task('make:favicons', () => {
   return gulp
     .src(`${config.favicons.source}/favicon.png`)
     .pipe(changed(`${config.favicons.build}`))
@@ -34,7 +37,7 @@ gulp.task('favicons', () => {
           opengraph: false,
           twitter: false,
           windows: true,
-          yandex: true
+          yandex: false
         },
         html: config.favicons.html,
         replace: true
@@ -42,15 +45,30 @@ gulp.task('favicons', () => {
     )
     .on('error', gutil.log)
     .pipe(gulp.dest(`${config.favicons.build}`))
-    .pipe(browser.stream())
-    .pipe(
-      gulpif(
-        config.enable.notify,
-        notify({
-          title: config.notify.title,
-          message: 'Favicons task complete',
-          onLast: true
-        })
-      )
-    );
+    .pipe(browser.stream());
 });
+
+gulp.task('favicons:fix', () => {
+  return gulp
+    .src(`${config.favicons.build}/*.{xml,json,webapp}`)
+    .on('error', gutil.log)
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
+    .pipe(replace('{{rootPath}}', '/'))
+    .pipe(gulp.dest(`${config.favicons.build}`));
+});
+
+gulp.task(
+  'favicons',
+  gulp.series('make:favicons', 'favicons:fix', done => {
+    if (config.enable.notify) {
+      notifier.notify({
+        title: config.notify.title,
+        message: 'Favicons task complete'
+      });
+    }
+
+    done();
+  })
+);
