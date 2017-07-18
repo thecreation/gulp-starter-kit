2fc299690eced14bf97ee4db78a17255bb2b4d17
amazingSurge <amazingSurge@yahoo.com>
2017-07-18 14:58:46 +0800

    update fonts

--- tasks/fonts.js
+++ tasks/fonts.js
@@ -9,7 +9,7 @@ import notify from 'gulp-notify';
 // ------------------
 gulp.task('fonts', () => {
   return gulp
-    .src(`${config.fonts.source}/**/*.+(woff2|woff|eot|ttf|svg)`)
+    .src(`${config.fonts.source}/**/*.+(css|woff2|woff|eot|ttf|svg)`)
     .pipe(changed(`${config.fonts.build}`))
     .pipe(gulp.dest(`${config.fonts.build}`))
     .pipe(browser.stream())
