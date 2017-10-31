af563d6eae33b519f6968417e25dc1d6ac9e9bb7
kaptinlin <amazingsurge@gmail.com>
2017-11-01 04:29:29 +0800

    update autoprefixuer config

--- postcss.config.js
+++ postcss.config.js
@@ -3,14 +3,7 @@ let config = require('./config');
 // See: https://github.com/postcss/postcss-loader#usage
 let postcssConfig = {
   plugins: [
-    require('autoprefixer')({
-      browsers: [
-        // For more browsers, see https://github.com/ai/browserslist
-        '> 1%',
-        'last 3 versions',
-        'IE >= 9'
-      ]
-    }),
+    require('autoprefixer')({ cascade: false }),
     require('stylefmt')
   ]
 };
